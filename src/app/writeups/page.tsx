9fdb2a54688b9f766ca1bded69b569b471d0a2dc
Mehmet Eren Alpan <alpanmemo@gmail.com>
2025-08-26 14:38:57 +0300

    Update page.tsx

--- src/app/writeups/page.tsx
+++ src/app/writeups/page.tsx
@@ -1,4 +1,4 @@
-import { allPosts } from "contentlayer/generated";
+import { allPosts } from ".contentlayer/generated";
 import PostCard from "@/components/PostCard";
 
 export const dynamic = 'error';
